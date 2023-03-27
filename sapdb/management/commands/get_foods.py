d14f4709420d3f522673b14b8db5c18e6b4389c9
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-03-27 01:48:55 -0700

    Connect to Planetscale and add foods from bs4

---
+++ sapdb/management/commands/get_foods.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import requests
+from sapdb.models import Food, Trigger, Pack
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Load foods into the database'
+
+    def handle(self, *args, **options):
+      turtle: Pack = Pack.objects.get(name="Turtle Pack")
+      puppy: Pack = Pack.objects.get(name="Puppy Pack")
+      star: Pack = Pack.objects.get(name="Star Pack")
+
+      foods = 'https://superautopets.fandom.com/wiki/Food'
+      response = requests.get(foods)
+      soup = BeautifulSoup(response.text, 'html.parser')
+
+      tbody = soup.find('tbody')
+      trows = tbody.select('tr')
+
+      for row in trows:
+        if row.select_one('td:nth-of-type(1)'):
+          tier = row.select_one('td:nth-of-type(1)').text.strip()
+          name = row.select_one('td:nth-of-type(2)').text.strip()
+          effect = row.select_one('td:nth-of-type(3)').text.strip()
+          in_turtle = row.select_one('td:nth-of-type(4)').text.strip()
+          in_puppy = row.select_one('td:nth-of-type(5)').text.strip()
+          in_star = row.select_one('td:nth-of-type(6)').text.strip()
+
+          already_present = Food.objects.filter(name=name).first()
+
+          if already_present:
+            # already_present.effect = already_present.effect.replace('\n', ' ')
+            # if "Yes" in in_turtle:
+            #   turtle.foods.add(already_present)
+            #   already_present.packs.add(turtle)
+
+            # if "Yes" in in_puppy:
+            #   puppy.foods.add(already_present)
+            #   already_present.packs.add(puppy)
+
+            # if "Yes" in in_star:
+            #   star.foods.add(already_present)
+            #   already_present.packs.add(star)
+            # print(already_present.effect)
+            # already_present.save()
+            continue
+
+          if type(tier) == str:
+            tier = 0
+
+          food = Food.objects.create(
+              tier=tier,
+              name=name,
+              image=f'{name}.webp',
+              effect=effect,
+          )
+
+          if in_turtle:
+            turtle.foods.add(food)
+
+          if in_puppy:
+            puppy.foods.add(food)
+
+          if in_star:
+            star.foods.add(food)
